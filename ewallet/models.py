9780b5c3aea109d6c5e9d48a312ced140ee0c0f3
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2018-10-12 14:47:37 +0300

    Split transfer method of Wallet in transactions

--- ewallet/models.py
+++ ewallet/models.py
@@ -1,16 +1,50 @@
+class NotEnoughFundsException(Exception):
+    """ Not enough funds in account. """
+
+
+class AccountLimitException(Exception):
+    """ Account limit is reached. """
+
+
 class Wallet(object):
     """ Digital wallet that allows an individual to make electronic transactions. """
 
     def __init__(self, owner, limit):
         self._owner = owner
-        self.limit = limit
-        self.balance = 0.0
+        self._limit = limit
+        self._balance = 0
+
+    def put(self, amount):
+        """ Put money into account. """
+        if self._balance + amount > self._limit:
+            raise AccountLimitException()
+        else:
+            self._balance += amount
+
+    def draw(self, amount):
+        """ Draw money from account. """
+        if self._balance - amount < 0:
+            raise NotEnoughFundsException
+        else:
+            self._balance -= amount
+
+    def _rollback(self, amount):
+        """ Rollback draw in case of error. """
+        self._balance += amount
 
     def transfer(self, other, amount):
         """ Transfer money to another account. """
-        if self.balance - amount < 0 or other.balance + amount > other.limit:
-            return False
-        else:
-            self.balance -= amount
-            other.balance += amount
-            return True
+        try:
+            self.draw(amount)
+            other.put(amount)
+        except AccountLimitException as e:
+            self._rollback(amount)
+            raise e
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @property
+    def owner(self):
+        return self._owner
