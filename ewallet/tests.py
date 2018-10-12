873c55418c815b3b0ad975fe74e224608e8d57d8
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2018-10-12 14:48:25 +0300

    Add tests

---
+++ ewallet/tests.py
@@ -0,0 +1,41 @@
+import unittest
+
+from .models import Wallet, NotEnoughFundsException, AccountLimitException
+
+
+class TestWallet(unittest.TestCase):
+    LIMIT = 100
+    LIMIT_2 = 50
+    DEFAULT_AMOUNT = 60
+
+    def setUp(self):
+        self.w1 = Wallet('Jim Beam', self.LIMIT)
+        self.w2 = Wallet('John Galt', self.LIMIT_2)
+        self.w1.put(self.DEFAULT_AMOUNT)
+
+    def test_put(self):
+        self.assertEqual(self.w1.balance, self.DEFAULT_AMOUNT)
+        with self.assertRaises(AccountLimitException):
+            self.w1.put(self.DEFAULT_AMOUNT)
+
+    def test_draw(self):
+        self.w1.draw(self.DEFAULT_AMOUNT)
+        self.assertEqual(self.w1.balance, 0)
+        with self.assertRaises(NotEnoughFundsException):
+            self.w1.draw(self.DEFAULT_AMOUNT)
+
+    def test_successful_transfer(self):
+        amount = 20
+        self.w1.transfer(self.w2, 20)
+        self.assertEqual(self.w2.balance, amount)
+        self.assertEqual(self.w1.balance, self.DEFAULT_AMOUNT - amount)
+
+    def test_transfer_not_enough_funds(self):
+        with self.assertRaises(NotEnoughFundsException):
+            self.w1.transfer(self.w2, self.LIMIT)
+
+    def test_transfer_rollback(self):
+        with self.assertRaises(AccountLimitException):
+            self.w1.transfer(self.w2, self.DEFAULT_AMOUNT)
+        self.assertEqual(self.w1.balance, self.DEFAULT_AMOUNT)
+        self.assertEqual(self.w2.balance, 0)
