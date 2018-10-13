1f7dd025d9cf1aea10e7aad98993c82b5cc6a112
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2018-10-13 22:12:36 +0300

    Add new commands to CLI

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # ewallet
+```commandline
+python cli.py
+Welcome to eWallet!
+Enter "help" to list available commands.
+> ?
+
+Available commands: (type help <command>):
+==========================================
+EOF  create_wallet  draw_money  help  put_money  show_wallets  transfer_money
+
+> create_wallet test1 10
+Done
+pk  owner  balance  limit
+0  test1  0.0  10.0
+> create_wallet test2 20
+Done
+pk  owner  balance  limit
+1  test2  0.0  20.0
+> put_money 1 19
+Done
+pk  owner  balance  limit
+1  test2  19.0  20.0
+> transfer_money 1 2 20
+Error: wallet is not found.
+> show_wallets
+pk  owner  balance  limit
+0  test1  0.0  10.0
+1  test2  19.0  20.0
+> transfer_money 1 0 20
+Error: not enough funds in account.
+> transfer_money 1 0 19
+Error: account limit is reached.
+> transfer_money 1 0 9.5
+Done
+pk  owner  balance  limit
+1  test2  9.5  20.0
+0  test1  9.5  10.0
+```
+
+## TODO
+* add table output
+* add argparse
--- ewallet/cli.py
+++ ewallet/cli.py
@@ -1,6 +1,6 @@
 from cmd import Cmd
 
-from models import Wallet
+from models import Wallet, WalletError
 
 
 class Cli(Cmd):
@@ -10,30 +10,82 @@ class Cli(Cmd):
         self.intro = 'Welcome to eWallet!\nEnter "help" to list available commands.'
         self.doc_header = 'Available commands: (type help <command>):'
 
-    def do_create_account(self, line):
-        """create_account owner limit\nCreate a new account in eWallet."""
+    def do_create_wallet(self, line):
+        """create_wallet owner limit\nCreate a new account in eWallet."""
         owner, limit = line.split()
-        w = Wallet(owner, limit)
-        self.stdout.write(
-            'Account created: {0}: limit = {1}'.format(w.owner, w.limit)
-        )
-        self.stdout.write('\n')
+        w = Wallet(owner, float(limit))
+        self.write('Done')
+        self.write_table(w.SHOW_PROPS, [w.prop_list])
 
-    def do_show_accounts(self, line):
+    def do_show_wallets(self, line):
         """show_accounts\nDisplay all existing eWallet accounts."""
         if not Wallet.objects:
             self.columnize([])
         else:
-            self.columnize(Wallet.SHOW_KEYS)
-            for _, wal in sorted(Wallet.objects.iteritems(), key=lambda (k, v): k):
-                self.columnize([str(getattr(wal, key)) for key in Wallet.SHOW_KEYS])
+            self.write_table(
+                Wallet.SHOW_PROPS,
+                [w.prop_list for _, w in sorted(Wallet.objects.iteritems(), key=lambda (k, v): k)]
+            )
+
+    def do_put_money(self, line):
+        """put_money wallet_id amount"""
+        w_id, amount = line.split()
+        w_id = int(w_id)
+        try:
+            w = Wallet.get_obj(w_id)
+            w.put(float(amount))
+            self.write('Done')
+            self.write_table(w.SHOW_PROPS, [w.prop_list])
+        except WalletError as e:
+            self.write('Error: {0}'.format(str(e)))
+
+    def do_draw_money(self, line):
+        """draw_money wallet_id amount"""
+        w_id, amount = line.split()
+        w_id = int(w_id)
+        try:
+            w = Wallet.get_obj(w_id)
+            w.draw(float(amount))
+            self.write('Done')
+            self.write_table(w.SHOW_PROPS, [w.prop_list])
+        except WalletError as e:
+            self.write('Error: {0}'.format(str(e)))
+
+    def do_transfer_money(self, line):
+        """transfer_money from_wallet_id to_wallet_id amount"""
+        from_w_id, to_w_id, amount = line.split()
+        from_w_id, to_w_id = int(from_w_id), int(to_w_id)
+        try:
+            from_w = Wallet.get_obj(from_w_id)
+            to_w = Wallet.get_obj(to_w_id)
+            from_w.transfer(to_w, float(amount))
+            self.write('Done')
+            self.write_table(
+                Wallet.SHOW_PROPS,
+                [from_w.prop_list, to_w.prop_list]
+            )
+        except WalletError as e:
+            self.write('Error: {0}'.format(str(e)))
 
     def do_EOF(self, line):
+        """EOF -- exit eWallet"""
         self.stdout.write('Exiting...')
         exit(0)
 
     def emptyline(self):
         pass
+
+    def write(self, s):
+        self.stdout.write(s)
+        self.stdout.write('\n')
+
+    def write_table(self, headers, rows):
+        if not rows:
+            self.columnize([])
+        else:
+            self.columnize(headers)
+            for row in rows:
+                self.columnize(row)
 
 
 if __name__ == "__main__":
